6bc98922a8cff180ede263dcde95cfc7d0011c4d
Vinhlee95 <lethanhvinh95@gmail.com>
2018-05-16 12:06:43 +0300

    fetch data

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import request from 'superagent';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-         <h2>Performer Info</h2>
-      </div>
-    );
-  }
+   constructor(props){
+      super(props);
+      this.state = {};
+   }
+
+   componentDidMount() {
+      request
+         .post('https://nodedev.gigleapp.com/user')
+         .send({
+            action: 'getPerformersAndProducts',
+            userId: 'ZuaqGwjNc6M47HchSJYVa2lunf03'
+         })
+         .end((err, res) => {
+            res ? this.setState({
+               performers: res.body
+            }) : console.log('Error fetching')
+         });
+   }
+   
+   render() {
+      console.log(this.state.performers)
+      return (
+         <div className="App">
+            <h2>Performer Info</h2>
+         </div>
+      );
+   }
 }
 
 export default App;
